20f36f5a192ef8d8d1a696ac2f3ac512a1157db3
dudumaroja <dudumaroja@gmail.com>
2025-12-09 21:46:33 -0300

    Refactor disconnection logic to nullify properties
    Set connection and settings properties to null instead of clearing them.

--- extension.js
+++ extension.js
@@ -88,10 +88,9 @@ export default class TwoWallpapersExtension extends Extension {
         if (this._windowAddedId) this._currentWs.disconnect(this._windowAddedId);
         if (this._windowRemovedId) this._currentWs.disconnect(this._windowRemovedId);
         this._disconnectFromCurrentWindows();
-        this._connIds.clear();
-        this._currentWs.clear();
-        this._settings.clear();
-        this._backgroundSettings.clear();
+        this._connIds = null;
+        this._currentWs = null;
+        this._settings = null;
+        this._backgroundSettings = null;
     }
 }
-
